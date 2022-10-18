d69f3eda6aded2bc07301eb433bd36310ab707ec
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-10-18 11:08:41 -0300

    treinando funçoes

---
+++ funcoes.py
@@ -0,0 +1,10 @@
+# python3
+# def pra definir a função
+# Como todo bloco de código em Python, o caractere : define o início do bloco, e a indentação define seu fim.
+
+def soma(x, y):
+    return x + y
+
+soma(2, 2)  # os parâmetros aqui são posicionais
+
+soma(x=2, y=2)  # aqui estamos nomeando os parâmetros
